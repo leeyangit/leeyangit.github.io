da90a9e0a88041d9723dbf2017afd974f5d91763
leyan <167346@supinfo.com>
2018-05-08 00:56:43 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+var myHeading = document.querySelector('h1');
+myHeading.innerHTML = 'Hello world!';
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefoximg.jpg') {
+      myImage.setAttribute ('src','images/firefoximg2.png');
+    } else {
+      myImage.setAttribute ('src','images/firefoximg.jpg');
+    }
+}
+var myButton = document.querySelector('button');
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
